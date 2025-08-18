651921300f7579f5dc771e1fce115b2e5fd50abf
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-19 01:11:32 +0200

    Create LatticesToRightCornerScreen.hlsl

---
+++ LatticesToRightCornerScreen.hlsl
@@ -0,0 +1,3 @@
+//as there are several description for sole tamari lattices
+//we wouldnt compute high digits numbers. then the kajiya rsuation may 
+//be used with a technique we would name tiling default 
