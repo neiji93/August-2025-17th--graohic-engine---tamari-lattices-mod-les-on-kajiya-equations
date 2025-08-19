8ed766ecc4b661999c4c8df58342299ada0c4dd1
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-19 02:11:43 +0200

    Update LatticesToRightCornerScreen.hlsl

--- LatticesToRightCornerScreen.hlsl
+++ LatticesToRightCornerScreen.hlsl
@@ -1,3 +1,7 @@
 //as there are several description for sole tamari lattices
 //we wouldnt compute high digits numbers. then the kajiya rsuation may 
 //be used with a technique we would name tiling default 
+//if we had only one rule, it would be best to change reflection models sometimed 
+//then, the light propagation zlgorithm could not be clamped.
+//so the top left corner and the bottom right corner of zcenes are different.
+//to model that, lets add another rule. then clearcoat becomes pbr.
