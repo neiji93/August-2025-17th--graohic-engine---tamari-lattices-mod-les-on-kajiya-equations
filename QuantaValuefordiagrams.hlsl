a02c648d0ea559f0229b192b901edb09aa97e7a4
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-20 07:30:29 +0200

    Create QuantaValuefordiagrams.hlsl

---
+++ QuantaValuefordiagrams.hlsl
@@ -0,0 +1,5 @@
+//to fail according to my last mean field game,
+//we have multiple choices. but we wont it. to prevent that
+//we need to increase the deadline of ray tracing 
+//then, lwe d better have to patch the first rays which is a diagram behaviour.
+//the proof is that diagrams are largemy used in physics.
